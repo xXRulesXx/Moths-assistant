a9e0ff879d002d1e11371d50e1a13d16749258a3
xXRulesXx <37060584+xXRulesXx@users.noreply.github.com>
2018-03-05 07:37:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,21 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const commando = require('discord.js-commando');
 
-Client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
-        message.reply('pong');
+class DiceRollCommand extends commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'roll',
+            group: 'random',
+            memberName: 'roll',
+            description: 'Rolls a die'
+        });
     }
-});
+
+    async run(message, args) {
+        var roll = Math.floor(Math.random() * 100) + 1;
+        message.reply("1d100=" + roll);
+    }
+}
+
+module.exports = DiceRollCommand;
 
 client.login(process.env.Bot_Token);
